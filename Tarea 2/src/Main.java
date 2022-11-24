6157473634d00797d4d69c036a65c9acea580d49
jiamtorres <119003088+jiamtorres@users.noreply.github.com>
2022-11-24 13:42:42 -0400

    Add files via upload

---
+++ Tarea 2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuerta();
+        System.out.println(miCoche.puertas);
+    }
+
+    static class Coche {
+    public int puertas = 4;
+    public void AgregarPuerta() { this.puertas++; }
+    }
+}
